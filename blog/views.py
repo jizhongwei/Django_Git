0c505534f90d3180b1d15a3718ea13f77658ddbc
dick <jzwxiaoniao@163.com>
2019-09-27 14:37:06 +0800

    在view.py视图文件中增加对markdown语法的支持

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 from .models import *
+import markdown
 
 
 def index(request):
@@ -10,10 +11,17 @@ def index(request):
 
 def detail(request, pk):
     post = get_object_or_404(Blog, pk = pk)
+    post.body = markdown.markdown(post.body,
+                                  extensions = [
+                                      'markdown.extensions.extra',
+                                      'markdown.extensions.codehilite',
+                                  ])
     return render(request, 'blog/detail.html', locals())
 
 def category(request, pk):
-    pass
+    cate = get_object_or_404(Category, pk = pk)
+    post_list = Blog.objects.filter(category= cate)
+    return render(request, 'blog/index.html' ,locals())
 
 def tag(request,pk):
     pass
@@ -22,4 +30,6 @@ def search(request):
     pass
 
 def archives(request, year, month):
-    pass
+    post_list = Blog.objects.filter(created_time__year= year,
+                                    created_time__month= month)
+    return render(request, 'blog/index.html', locals())
